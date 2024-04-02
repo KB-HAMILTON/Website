6792023a252acc8c8fb2cb19334268755d73bf6d
KB-HAMILTON <155551711+KB-HAMILTON@users.noreply.github.com>
2024-04-01 21:07:39 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -9,7 +9,7 @@
     $Password =$_POST['Password'];
 
 
-  $conn = new mysqli('localhost','root','','registration');
+  $conn = new mysqli('127.0.0.1','root','','registration');
   if ($conn->connect_error){
     die("Connection failed: " .$conn->connect_error);
   }else{
